717f041ffb411d5d578deaa9e9b4970690c6549e
minalnarsale <minalnarsale@gmail.com>
2021-11-16 23:57:32 +0530

    basic functionality

---
+++ routes.js
@@ -0,0 +1,33 @@
+let express = require(`express`);
+const router = express.Router();
+
+router.post(`/accountTypeChecker`, (req, res) => {
+  
+    let accountType = accountTypeChecker(req.body.accountBalanceHistory);
+    let message = accountType === 'A' ? 
+    "The account in which the balance amount decreases by varying amounts each month." 
+    : "The account in which the balance amount changes by the same amount each month."
+    res.send({"AccountType": accountType, "message": message});
+});
+
+const accountTypeChecker = (accountBalanceHistory) => {
+    
+    let newRecord = [];
+    accountBalanceHistory.forEach(element => {
+
+        newRecord[element.monthNumber] = element.account.balance.amount;
+    });
+    let diff = newRecord[1] - newRecord[0];
+    let varyingAmountType = false;
+    for(let i=newRecord.length-1; i>0; i--) {
+        let newdiff = newRecord[i] - newRecord[i-1];
+        if(newdiff !== diff) {
+            varyingAmountType = true;
+            break;
+        }
+    }
+    return varyingAmountType ? "A" : "B";
+  };
+
+module.exports = {router, accountTypeChecker};
+
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require(`express`);
+const routes = require(`./routes`);
+
+let app = express();
+
+//middleware: separation of incoming request into req.header, req.body
+app.use(express.urlencoded({extended: true}));
+
+//middleware: req.body -> Json object conversion
+app.use(express.json());
+
+//middleware: adding logger for each request coming to server
+app.use((req, res, next) => {
+
+    console.log(new Date(), req.method, (decodeURIComponent(req.url)));
+    next();
+});
+
+//defining routes for API
+app.use('/', routes);
+
+//routes get execut in order so `/*` route is at last 
+app.get('/*', (req, res) => {
+
+    res.send({'msg': 'wrong route send'});
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
